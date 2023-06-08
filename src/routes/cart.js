1939c15127eb6f7469df2e3d44d5745c838e7caa
DucNguyen <89559446+Nguyenduc2k3@users.noreply.github.com>
2023-06-08 21:50:12 +0700

    Update cart.js

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   getAll,
+  getDetail,
   create,
   update,
   remove,
@@ -8,6 +9,7 @@ import {
 const router = express.Router();
 
 router.get("/", getAll);
+router.get("/:id", getDetail);
 router.post("/", create);
 router.put("/:id", update);
 router.delete("/:id", remove);
